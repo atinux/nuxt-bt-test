eba82d05aa13ea9fcfc695d45c2c2ace15fd4de1
Clark Du <clark.du@betbright.com>
2018-08-01 16:25:48 +0100

    refactor: load components as needed

--- plugins/bootstrap-vue.js
+++ plugins/bootstrap-vue.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import { Button, Modal } from 'bootstrap-vue/es/components';
+import Button from 'bootstrap-vue/es/components/button';
+import Modal from 'bootstrap-vue/es/components/modal';
 
 Vue.use(Button);
 Vue.use(Modal);
